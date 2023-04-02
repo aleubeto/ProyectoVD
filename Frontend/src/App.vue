97e11580c2952269fbcfb010256aa678aba7c79c
ale_wsl <alejo.ubeto@gmail.com>
2023-04-01 19:10:22 -0600

    Reorganización de archivos

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/views/HelloWorld.vue'
+import TheWelcome from './components/views/TheWelcome.vue'
 </script>
 
 <template>
--- Frontend/src/components/views/TheWelcome.vue
+++ Frontend/src/components/views/TheWelcome.vue
@@ -1,10 +1,10 @@
 <script setup>
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import DocumentationIcon from '../icons/Isologo/Icon_Isologo-White.vue'
+import ToolingIcon from '../icons/Isotipo/Icon_Isotipo-white.vue'
+import EcosystemIcon from '../icons/Isologo/Icon_Isologo-color.vue'
+import CommunityIcon from '../icons/Isologo/Icon_Isologo-Black.vue'
+import SupportIcon from '../icons/Isotipo/Icon_Isotipo-black.vue'
 </script>
 
 <template>
