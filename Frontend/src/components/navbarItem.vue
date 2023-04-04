e36818f69834de7b6772ce7e82581fc85a219eee
ale_wsl <alejo.ubeto@gmail.com>
2023-04-03 23:35:22 -0600

    NavBar component done

---
+++ Frontend/src/components/navbarItem.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <slot name="icon"></slot>
+            <a class="navbar-brand" href="#"><slot name="heading"></slot></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link v-for="(navItem, index) in navItems" :key="index" :to="navItem.route" class="nav-link">{{ navItem.text }}</router-link>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+        props: {
+            navItems: {
+                type: Array,
+                required: true
+                }
+        }
+    }
+</script>
