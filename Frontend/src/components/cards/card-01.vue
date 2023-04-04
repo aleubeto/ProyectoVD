34c294209e1399487756a812a2dc99dcfb6c5c92
ale_wsl <alejo.ubeto@gmail.com>
2023-04-04 14:18:47 -0600

    Creación de componente Card-01

---
+++ Frontend/src/components/cards/card-01.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <slot name="image"></slot>
+        <div class="card-body">
+            <h5 class="card-title"><slot name="cardTitle"></slot></h5>
+            <p class="card-text"><slot name="cardText"></slot></p>
+        </div>
+    </div>
+</template>
+
+<style>
+.card{
+    width: 14rem;
+}
+</style>
