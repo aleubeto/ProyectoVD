f7f4b91de6e11c64da2dc796d602a8cf5e0c05b7
ale_wsl <alejo.ubeto@gmail.com>
2023-04-04 19:36:50 -0600

    Separación de elementos de nav

--- Frontend/src/components/navbarItem.vue
+++ Frontend/src/components/navbarItem.vue
@@ -5,15 +5,19 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div class="container">
-            <slot name="icon"></slot>
-            <a class="navbar-brand" href="#"><slot name="heading"></slot></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <router-link v-for="(navItem, index) in navItems" :key="index" :to="navItem.route" class="nav-link">{{ navItem.text }}</router-link>
+            <div class="right-section">
+                <slot name="icon"></slot>
+                <a class="navbar-brand" href="#"><slot name="heading"></slot></a>
+            </div>
+            <div class="left-section">
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <router-link v-for="(navItem, index) in navItems" :key="index" :to="navItem.route" class="nav-link">{{ navItem.text }}</router-link>
+                    </div>
                 </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
         </div>
     </nav>
@@ -30,3 +34,10 @@
         }
     }
 </script>
+
+<style>
+.container{
+    display: flex;
+    justify-content: space-between;
+}
+</style>
