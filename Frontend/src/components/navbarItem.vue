37a115e00710fdc91e7d557e275343d39a885e5d
ale_wsl <alejo.ubeto@gmail.com>
2023-04-08 21:46:17 -0600

    Correción de navbar button

--- Frontend/src/components/navbarItem.vue
+++ Frontend/src/components/navbarItem.vue
@@ -10,14 +10,14 @@
                 <a class="navbar-brand" href="#"><slot name="heading"></slot></a>
             </div>
             <div class="left-section">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <router-link v-for="(navItem, index) in navItems" :key="index" :to="navItem.route" class="nav-link">{{ navItem.text }}</router-link>
                     </div>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
             </div>
         </div>
     </nav>
