ec6f045dc7a1c79d64bd1c420dd157ce3887edb0
ale_wsl <alejo.ubeto@gmail.com>
2023-04-14 12:51:54 -0600

    [DATABASE] admin profile created for database CRUD

--- Backend/sparkHorizon/admin.py
+++ Backend/sparkHorizon/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TeamMember, Gender, Usuario, Post, Calificacion
 
 # Register your models here.
+admin.site.register(TeamMember)
+admin.site.register(Gender)
+admin.site.register(Usuario)
+admin.site.register(Post)
+admin.site.register(Calificacion)
