043f33ca63eaf823a8bcb1688620190890d5327a
ale_wsl <alejo.ubeto@gmail.com>
2023-04-21 10:59:42 -0600

    [ROUTES] routes for each model

--- Backend/sparkHorizon/urls.py
+++ Backend/sparkHorizon/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework import routers
-from .views import PostViewSet
+from .views import *
 
 # Router que renderiza PostViewSet en la ruta "/api/posts"
 router = routers.DefaultRouter()
 router.register('posts', PostViewSet)
+router.register('genders', GenderViewSet)
+router.register('usuarios', UsuarioViewSet)
+router.register('team', TeamViewSet)
+router.register('calificaciones', CalificacionViewSet)
 urlpatterns = router.urls
