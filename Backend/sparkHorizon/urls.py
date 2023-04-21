f5583e944c06e0e1779b64424e047d6586ef1bd0
ale_wsl <alejo.ubeto@gmail.com>
2023-04-21 10:51:43 -0600

    [VIEWS] router register PostView ad /api/posts

--- Backend/sparkHorizon/urls.py
+++ Backend/sparkHorizon/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import index
-urlpatterns = [
-    path('', index, name='index')
-]
+from rest_framework import routers
+from .views import PostViewSet
+
+# Router que renderiza PostViewSet en la ruta "/api/posts"
+router = routers.DefaultRouter()
+router.register('posts', PostViewSet)
+urlpatterns = router.urls
