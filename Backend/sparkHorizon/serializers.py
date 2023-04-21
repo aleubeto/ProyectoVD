3438f7764105cdee10ee403d67abccb9c08aa9fd
ale_wsl <alejo.ubeto@gmail.com>
2023-04-21 10:54:46 -0600

    [MISC] serializer for each model

--- Backend/sparkHorizon/serializers.py
+++ Backend/sparkHorizon/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework.serializers import ModelSerializer, ALL_FIELDS
-from .models import Post
+from .models import Post,Gender,Usuario,TeamMember,Calificacion
 
 class PostSerializer(ModelSerializer):
     class Meta:
         model: Post
+        fields: ALL_FIELDS
+
+class GenderSerializer(ModelSerializer):
+    class Meta:
+        model: Gender
+        fields: ALL_FIELDS
+
+class UsuarioSerializer(ModelSerializer):
+    class Meta:
+        model: Usuario
+        fields: ALL_FIELDS
+
+class TeamSerializer(ModelSerializer):
+    class Meta:
+        model: TeamMember
+        fields: ALL_FIELDS
+
+class CalificacionSerializer(ModelSerializer):
+    class Meta:
+        model: Calificacion
         fields: ALL_FIELDS
