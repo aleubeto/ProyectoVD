5ba636b017e48350c2cf0d6a9bfb776a4f55dd7f
ale_wsl <alejo.ubeto@gmail.com>
2023-05-03 17:45:50 -0600

    [FIXED] onMounted Hook

--- Frontend/src/views/AboutUsView.vue
+++ Frontend/src/views/AboutUsView.vue
@@ -23,7 +23,7 @@
         }
     ]);
 
-    onMounted(
+    onMounted( () => {
         axios.get('http://localhost:8000/api/posts')
         .then((result) => {
             console.log(result.data);
@@ -32,25 +32,15 @@
         .catch((error) => {
             console.log(error)
         })
-    )
-    /*const data = ref([{
-        titulo: "Bootstrap, crea sitios responsivos facilmente", portada: "/src/assets/img/bootstrap.png"
-    },
-    {
-        titulo: "La magia de SCRUM, el secreto de su metodología", portada: "/src/assets/img/scrum.jpg"
-    },
-    {
-        titulo: "Vue vs React, la guerra de los frameworks", portada: "/src/assets/img/vue-vs-react.png"
-    },
-    ])*/
+    })
 </script>
 
 <!--Template de vista-->
 <template>
     <main>
         <h2 class="mt-5 mb-4">Blog</h2>
-        <div class="cards">
-            <Card v-for="(item, index) in data" v-bind:key="index">
+        <div v-if="data.length" class="cards">
+            <Card v-for="(item, index) in data" :key="index">
                 <template #image><img :src="item.portada" class="card-img-top" alt="..."/></template>
                 <template #cardTitle>{{item.titulo}}</template>
                 <CustomButton>Check this out</CustomButton>
